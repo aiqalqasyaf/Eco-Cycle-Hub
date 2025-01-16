49b4bcdacf3b8820ace8931da68d07e48f82cb76
aiqalqasyaf <qasyaf@gmail.com>
2025-01-16 20:39:45 +0800

    Update LoginBean.java

--- bean/LoginBean.java
+++ bean/LoginBean.java
@@ -8,6 +8,9 @@ public class LoginBean implements java.io.Serializable {
     private String username;
     private String password;
     private String role;
+    private String email;
+    private String phone;
+    private String address;
 
     // Getters and setters
     public String getUsername() {
@@ -33,5 +36,31 @@ public class LoginBean implements java.io.Serializable {
     public void setRole(String role) {
         this.role = role;
     }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
 }
 
